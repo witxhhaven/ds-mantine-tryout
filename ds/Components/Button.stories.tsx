07b81e8e0a26101e12405370524e78f9ee8dd7af
Alvin Leu <alvin_leu@tech.gov.sg>
2026-01-03 08:04:26 +0800

    remove content

--- ds/Components/Button.stories.tsx
+++ ds/Components/Button.stories.tsx
@@ -1,8 +1,7 @@
 import type { Story, StoryDefault } from "@ladle/react";
-import { ButtonShowcase } from "./Button";
 
 export default {
   title: "`02-Components`",
 } satisfies StoryDefault;
 
-export const Button: Story = () => <ButtonShowcase />;
+export const Button: Story = () => <>put button content</>;
--- ds/Components/FancyButton.stories.tsx
+++
@@ -1,8 +0,0 @@
-import type { Story, StoryDefault } from "@ladle/react";
-import { FancyButtonShowcase } from "./FancyButton";
-
-export default {
-  title: "`02-Components`",
-} satisfies StoryDefault;
-
-export const FancyButton: Story = () => <FancyButtonShowcase />;
